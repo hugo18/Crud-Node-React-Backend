a4d6c5d01c252d976d5c29ca84f63d79b5d7c80b
Hugo Alves Silva <hugoalves@egresso.ufg.br>
2025-02-13 17:14:07 -0300

    Backend pronto

--- src/controllers/clientController.js
+++ src/controllers/clientController.js
@@ -16,7 +16,52 @@ export const createClient = async (req, res) => {
         const newClient = await clientService.createClient(clientData);
         res.status(200).json(newClient);
     } catch (err) { 
-        console.error('Error adding client:', err);
-        res.status(500).json({ message: 'Internal Server Error' });
+        console.error('Erro ao adicionar cliente:', err);
+        res.status(500).json({ message: 'Erro no servidor' });
     }
 };
+
+export const updateClient = async (req, res) => {
+    try {
+        const clientId = req.params.id;
+        const clientData = req.body;
+        const updatedClient = await clientService.updateClient(clientId, clientData);
+        if (!updatedClient) {
+            return res.status(404).json({ message: 'Cliente não encontrado' });
+        }
+        res.status(200).json(updatedClient);
+
+    } catch (err) { 
+        console.error('Erro ao atualizar cliente:', err);
+        res.status(500).json({ message: 'Erro no servidor' });
+    }
+};
+
+export const deleteClient = async (req, res) => {
+    try {
+        const clientId = req.params.id;
+        const deleted = await clientService.deleteClient(clientId);
+        if (!deleted) {
+        return res.status(404).json({ message: 'Cliente não encontrado' });
+        }
+
+        res.status(200).send();
+
+    } catch (err) { 
+        console.error('Erro ao apagar cliente:', err);
+        res.status(500).json({ message: 'Erro no servidor' });
+    }
+};
+
+/*
+export const searchClients = async (req, res) => {
+    try {
+      const searchTerm = req.query.q; // Get the search term from the query parameters
+      const clients = await clientService.searchClients(searchTerm);
+      res.status(200).json(clients);
+    } catch (error) {
+      console.error('Error ao pesquisar clientes:', error);
+      res.status(500).json({ message: 'Erro no servidor' });
+    }
+};
+*/
--- src/services/clientSevice.js
+++ src/services/clientSevice.js
@@ -15,3 +15,29 @@ export const createClient = async (clientData) => {
     
     return rows[0];
 }
+
+export const updateClient = async (clientId, clientData) => {
+    const { name, email, job, rate, isactive } = clientData;
+  
+    const { rows } = await query(
+      `UPDATE clients_tb SET name = $1, email = $2, job = $3, rate = $4, isactive = $5 
+       WHERE id = $6 RETURNING *`,
+      [name, email, job, rate, isactive, clientId]
+    );
+  
+    return rows[0];
+  };
+
+  export const deleteClient = async (clientId) => {
+    const { rowCount } = await query(`DELETE FROM clients_tb WHERE id = $1`, [clientId]);
+    return rowCount > 0; // Returns true if a row was deleted, false otherwise
+};
+/*
+export const searchClients = async (searchTerm) => {
+    const { rows } = await query(
+      `SELECT * FROM clients_tb WHERE name ILIKE $1 OR email ILIKE $1 OR job ILIKE $1`,
+      [`%${searchTerm}%`]
+    );
+    return rows;
+};
+*/
